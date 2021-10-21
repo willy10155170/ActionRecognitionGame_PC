6d73280b6744c05a1dc6d3e55d195023c4bbf09d
Andy000002 <52848853+Andy000002@users.noreply.github.com>
2021-10-22 01:28:22 +0800

    [feature] add is_gaming function

--- unity_skill.py
+++ unity_skill.py
@@ -1,15 +1,17 @@
 import socket
 import numpy as np
+from time import sleep
 class unity:
     def __init__(self,host="127.0.0.1",port=12333):
         self.host = host
         self.port = port
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+        self.is_gaming = False
     def connect(self):
         self.client.connect((self.host, self.port))
     
     def close(self):
+        self.client.send(bytes("end","utf-8"))
         self.client.close()
     
     def skill(self,skill):
@@ -24,14 +26,15 @@ class unity:
         '''
         self.client.send(bytes(skill,"utf-8"))
 
-    def _test(self):
-        print(self.client)
-        self.client.send(bytes("Hello World!","utf-8"))
-        
+    def is_gaming(self):
+         return self.is_gaming
+
 if __name__ == "__main__":
     uni = unity() # 宣告 unity(host,port)
-    uni.connect() # socket connect
-    uni.skill("light_punch")
-    uni.skill("damaged")
-    uni.skill("lose")
+    uni.connect()
+    msg = 'punch'
+    while(msg!='end'):
+        uni.skill(msg)
+        print(uni.is_gaming)
+        msg = input()
     uni.close() # socket close
