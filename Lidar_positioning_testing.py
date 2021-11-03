afe9470d0471d87fb1754dc6bd4417fda6f0641b
Ray-netsap <fk951238pop@gmail.com>
2021-11-04 05:29:48 +0800

    fix_return_data_not_empty_but_zero

--- Lidar_positioning_testing.py
+++ Lidar_positioning_testing.py
@@ -15,7 +15,42 @@ import winsound
 import queue
 
 
-History_joint = [
+History_joint1 = [
+    [0, -0.009208951145410538, -0.77310711145401, 2.2537500858306885],
+    [1, 0.007155116647481918, -0.800692617893219, 2.2775001525878906],
+    [2, 0.020479582250118256, -0.8099214434623718, 2.2945001125335693],
+    [3, 0.03380057215690613, -0.8110517263412476, 2.2977499961853027],
+    [4, -0.03239806741476059, -0.7979946136474609, 2.2790000438690186],
+    [5, -0.0455860011279583, -0.7947496771812439, 2.2790000438690186],
+    [6, -0.05874336138367653, -0.7910668253898621, 2.277750015258789],
+    [7, 0.060051921755075455, -0.7798907160758972, 2.283250093460083],
+    [8, -0.06975442916154861, -0.770786464214325, 2.3142499923706055],
+    [9, 0.02341325208544731, -0.733429491519928, 2.2502501010894775],
+    [10, -0.02541416510939598, -0.7254341840744019, 2.2452502250671387],
+    [11, 0.20709067583084106, -0.5538091659545898, 2.254500150680542],
+    [12, -0.13951700925827026, -0.5382019281387329, 2.24150013923645],
+    [13, 0.3656696677207947, -0.3451029658317566, 2.277750015258789],
+    [14, -0.22960935533046722, -0.3287741243839264, 2.3192501068115234],
+    [15, 0.46662020683288574, -0.12980696558952332, 2.283750057220459],
+    [16, -0.2917855381965637, -0.08647804707288742, 2.2715001106262207],
+    [17, 0.5051304697990417, -0.05679616704583168, 2.293750047683716],
+    [18, -0.3253891170024872, -0.009990726597607136, 2.274749994277954],
+    [19, 0.4641883969306946, -0.0529751181602478, 2.2710001468658447],
+    [20, -0.30585965514183044, 0.0033318321220576763, 2.277250051498413],
+    [21, 0.43974190950393677, -0.07262998819351196, 2.2632501125335693],
+    [22, -0.27046912908554077, -0.01675511710345745, 2.286750078201294],
+    [23, 0.14860232174396515, -0.07846488803625107, 2.058000087738037],
+    [24, -0.05337410047650337, -0.07824794948101044, 2.0502500534057617],
+    [25, 0.16298474371433258, 0.33378151059150696, 2.0965001583099365],
+    [26, -0.06955010443925858, 0.31793275475502014, 2.090250015258789],
+    [27, 0.17736251652240753, 0.6620067954063416, 1.9720001220703125],
+    [28, -0.11580567806959152, 0.654568076133728, 2.0082499980926514],
+    [29, 0.16654875874519348, 0.6867295503616333, 1.9155000448226929],
+    [30, -0.10808215290307999, 0.6887651681900024, 1.9750001430511475],
+    [31, -0.10808215290307999, 0.6887651681900024, 1.9750001430511475],
+    [32, -0.10808215290307999, 0.6887651681900024, 1.9750001430511475],
+]
+History_joint2 = [
     [0, -0.009208951145410538, -0.77310711145401, 2.2537500858306885],
     [1, 0.007155116647481918, -0.800692617893219, 2.2775001525878906],
     [2, 0.020479582250118256, -0.8099214434623718, 2.2945001125335693],
@@ -100,13 +135,19 @@ class poseDetector():
 # In[3]:
 
 
-def W_cor_Erfix(World_cor):
+def W_cor_Erfix(World_cor,Direc):
     for i in range(33):
         if(World_cor[i][3] == 0.0):
-            World_cor[i] = History_joint[i]
+            if Direc == 1:
+                World_cor[i] = History_joint1[i]
+            else:
+                World_cor[i] = History_joint2[i]
         else:
-            History_joint[i] = World_cor[i]
-    return World_cor,History_joint
+            if Direc == 1:
+                History_joint1[i] = World_cor[i]
+            else:
+                History_joint2[i] = World_cor[i]
+    return World_cor
 
 
 # In[4]:
@@ -154,7 +195,7 @@ def Cor_Normalization(JointSet):
 # In[6]:
 
 
-def Cal(image,lmList,aligned_depth_frame,color_intrin,ht):
+def Cal(image,lmList,aligned_depth_frame,color_intrin,ht,Direc):
     JointSet = []
     JointDepth = []
     World_cor = []
@@ -168,7 +209,10 @@ def Cal(image,lmList,aligned_depth_frame,color_intrin,ht):
         if len(lmList) !=0:
             for i in range(33):
                 try:
-                    depth = aligned_depth_frame.get_distance(int(lmList[i][1]),int(lmList[i][2]))
+                    if Direc == 1:
+                        depth = aligned_depth_frame.get_distance(int(lmList[i][1]+480),int(lmList[i][2]))
+                    else:
+                        depth = aligned_depth_frame.get_distance(int(lmList[i][1]),int(lmList[i][2]))
                     dx ,dy, dz = rs.rs2_deproject_pixel_to_point(color_intrin, [int(lmList[i][1]),int(lmList[i][2])], depth)
                     distance = math.sqrt(((dx)**2) + ((dy)**2) + ((dz)**2))
                 except:
@@ -176,7 +220,7 @@ def Cal(image,lmList,aligned_depth_frame,color_intrin,ht):
 
                 World_cor.append([i,dx,dy,dz])
 
-            World_cor, History_joint = W_cor_Erfix(World_cor)
+            World_cor = W_cor_Erfix(World_cor,Direc)
 
 #             for fp in World_cor:
 #                 fp = str(fp)
@@ -295,8 +339,11 @@ class frame_queue():
         align_to = rs.stream.color
         align = rs.align(align_to)
     
-        detector = poseDetector()
+        right_detector = poseDetector()
+        left_detector = poseDetector()
         pFps = 0
+        start = time.time()
+        end = 0
         #file = open('C:\\Users\\Ray\\Desktop\\NTOU\\Graduation_Project\\BodyData\\Test_Data.txt','w')
         ht = 0
     
@@ -331,40 +378,74 @@ class frame_queue():
                 grey_color = 153
                 depth_image_3d = np.dstack((depth_image,depth_image,depth_image))
                 #depth image is 1 channel, color is 3 channels
-                bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0),grey_color, color_image)
+                # bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0),grey_color, color_image)
     
                 depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
     
-    
-    
-                image = detector.findPose(bg_removed)
-                lmList = detector.findPosition(image)
-                height,width,channel = image.shape
-                total = height+width
-    
+                
+                bg_right = color_image[0:540,0:480,0:3]
+                bg_left = color_image[0:540,480:960,0:3]
+                
+                image1 = left_detector.findPose(bg_left)
+                image2 = right_detector.findPose(bg_right)
+                lmList1 = left_detector.findPosition(bg_left)
+                lmList2 = right_detector.findPosition(bg_right)
+                
+                
+#                 image = detector.findPose(bg_removed)
+#                 lmList = detector.findPosition(image)
+#                 height,width,channel = image.shape
+#                 total = height+width
+
                 JointSet = []
-                his_joint = np.zeros((13,4))
-    
-                try:
-                    if len(lmList) !=0:
-    
-                        JointSet = Cal(image,lmList,aligned_depth_frame,color_intrin,ht)
+                JointSet1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+                JointSet2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+                bg_right = color_image[0:540,0:480,0:3]
+                bg_left = color_image[0:540,480:960,0:3]
+
+                
+                image1 = left_detector.findPose(bg_left)
+                image2 = right_detector.findPose(bg_right)
+                lmList1 = left_detector.findPosition(bg_left)
+                lmList2 = right_detector.findPosition(bg_right)
+                
+                try:        
+                    if len(lmList1) !=0:
+                        JointSet1 = Cal(bg_left,lmList1,aligned_depth_frame,color_intrin,ht,0)
+    #                 else:
+    #                     JointSet1 = []
+                    if len(lmList2) !=0:
+                        JointSet2 = Cal(bg_right,lmList2,aligned_depth_frame,color_intrin,ht,1)
+    #                 else:
+    #                     JointSet2 = []
+                    end = time.time()
+                    ht = end-start
     
                 except Exception as e:
                     print(e)
                     pass
-            
-                if len(JointSet) != 0:
-                    self.frame_data.put(JointSet)
-    
+                
+
+                
+                
+                JointSet.append(JointSet1)
+                JointSet.append(JointSet2)
+                self.frame_data.put(JointSet)
+
+                if ht >= 90:
+                    cv2.destroyAllWindows()
+                
                 ht += 1
                 cFps = time.time()
                 fps = 1/(cFps-pFps)
                 pFps = cFps
     
-                cv2.putText(image, str(int(fps)), (70,50), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0), 3)
+                rv = np.hstack([image2,image1])
+
+                cv2.putText(rv, str(int(ht)), (70,50), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0), 3)
     
-                cv2.imshow('MediaPipe Pose', image)
+                cv2.imshow('MediaPipe Pose', rv)
     
                 key = cv2.waitKey(1)
                 if key & 0xFF == ord('q') or key == 27:
@@ -373,6 +454,9 @@ class frame_queue():
     
                 if self.game_status.empty() is False:
                     status = self.game_status.get()
+                else:
+                    status = False
+
                 if status is True:
                     break
     
